4c6a768c8ec6739927f9c90aecf1f66afba79d56
Michelle Perez <termysue1222@yahoo.com>
2016-01-28 10:44:40 -0600

    rps4.rb

--- rps4.rb
+++ rps4.rb
@@ -1,28 +1,28 @@
 win = 0.to_i 
 loses = 0.to_i
 
+3.times do 
 puts "choose r, p or s"
-user_input = gets.chomp.downcase
+player = gets.chomp.downcase
+
 computer = [ 'r', 'p', 's'].sample
 
-3.times do 
-  [player, computer] = 
-
-
-
-#Three time do iterate
-  #use case statement with [player, computer] (This will be challenging, but totally worth it!)
-    #when ['p', 'r'], ect..
-      #puts you win
-      # add one to win variable
-    #When ['r','r'], ect..
-      #Puts you tied
-    #Else
-      #You Lose
-      # add one to win variable
-
-#Declare Winner at end with score of wins and loses
-
-#### BONUS ####
-  #Show history of computer and user moves at end
-
+  case [player, computer]
+when ['p', 'r'] , ['r', 's'] , ['s', 'p']
+  win = win + 1.0
+  puts "You won!"
+when ['r', 'r'] , ['s', 's'] , ['p', 'p']
+  puts "You tied!"
+when ['p', 's'] , ['s', 'r'] , ['r', 'p']
+  puts "You lose!"
+  loses = loses + 1.0 
+end 
+puts "You have won #{win} and lost #{loses} game(s)"
+end 
+if win > loses
+   puts "You won!"
+elsif loses > win
+      puts "Oh no! You lost!"
+elsif win = loses 
+      puts "YOU drew!"
+end 
